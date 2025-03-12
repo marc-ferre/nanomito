22e476b05b11fd19a1502e592b54cfae7c1ca2e5
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2025-03-12 16:12:38 +0100

    Update version format.

--- wf-modmito.sh
+++ wf-modmito.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=END,FAIL,INVALID_DEPEND,REQUEUE,STAGE_OUT,TIME_LIMIT_90
 #SBATCH --mail-user=marc.ferre@univ-angers.fr
 
-VERSION='25-03-12.3'
+VERSION='25.03.12.3'
 
 AUTHOR='Marc FERRE <marc.ferre@univ-angers.fr>'
 
