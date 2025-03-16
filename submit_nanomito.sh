6b2b7a2026cfc56d9aa332a9448b091c43c00a3c
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2025-03-16 15:10:54 +0100

    Various improvements.

--- submit_nanomito.sh
+++ submit_nanomito.sh
@@ -1,44 +1,63 @@
 #!/bin/bash
 #
-# Submit workflows to Slurm
+# Submit Nanomito workflows to Slurm
 #
-# submit_nanomito.sh /Path/to/FastQ/dir/
+# submit_nanomito.sh /Path/to/run/dir/
 #
 set -e
 
 WF_DEMULTMT='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/workflows/wf-demultmt.sh'
 WF_MODMITO='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/workflows/wf-modmito.sh'
 
+FASTQ_DIR='fastq_pass'
+POD5_DIR='pod5'
+PROCESS_DIR='processing'
+
+MAIL_TYPE_CUSTOM='END,FAIL,INVALID_DEPEND,REQUEUE,STAGE_OUT,TIME_LIMIT_90'
+MAIL_TYPE_ALL='ALL'
+MAIL_TYPE_NONE='NONE'
+
+MAIL_USER='marc.ferre@univ-angers.fr'
+
 if [ $# -eq 0 ]
 	then
 		echo "[ERROR] No arguments supplied"
 		exit 9999 # die with error code 9999
 fi
-
-echo "Submitting workflows to Slurm..."
 cd $1
-# FASTQ_DIR_PATH=`pwd`
-# echo $FASTQ_DIR_PATH
-SAMPLE_ID=${PWD##*/}
-SAMPLE_ID=${SAMPLE_ID:-/} # to correct for the case where PWD is / (root)
-echo "Sample id : $SAMPLE_ID"
 
-cd ../../
-RUN_DIR_PATH=`pwd`
-echo "Run dir   : $RUN_DIR_PATH"
-cd $RUN_DIR
+echo "=== Submit workflows to Slurm ==="
 
-OUT_DIR="processing/$SAMPLE_ID"
-echo "Output dir: $OUT_DIR"
+RUN_PATH=`pwd`
+FASTQ_PATH="$RUN_PATH/$FASTQ_DIR"
+POD5_PATH="$RUN_PATH/$POD5_DIR"
+PROCESS_PATH="$RUN_PATH/$PROCESS_DIR"
 
-SLURM_PRE="slurm-$SAMPLE_ID"
-SLURM_EXT="txt"
+echo "Run path   : $RUN_PATH"
+echo "FastQ path : $FASTQ_PATH"
+echo "Pod5 path  : $POD5_PATH"
+echo "Output path: $PROCESS_PATH"
 
-SLURM_FILE="$RUN_DIR_PATH/$OUT_DIR/$SLURM_PRE.demultmt.$SLURM_EXT"
-JOBID=$(sbatch --parsable --chdir=$RUN_DIR_PATH --job-name="d${SAMPLE_ID: -5}" --output="$SLURM_FILE" $WF_DEMULTMT $SAMPLE_ID)
-echo "Submitted batch job $JOBID"
-echo "> Output in $SLURM_FILE"
+cd $FASTQ_PATH
+for DIR in $(ls -1); do
+	SAMPLE_ID=`basename $DIR`
+	echo "--- Sample: $SAMPLE_ID"
+	
+	OUT_PATH="$PROCESS_PATH/$SAMPLE_ID"
+	
+	SLURM_PRE="slurm-$SAMPLE_ID"
+	SLURM_EXT='txt'
+	
+	WF_ID='demultmt'
+	SLURM_FILE="$OUT_PATH/$SLURM_PRE.$WF_ID.$SLURM_EXT"
+	JOBID=$(sbatch --parsable --chdir="$RUN_DIR_PATH" --job-name="${WF_ID:0:1}${SAMPLE_ID: -5}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_CUSTOM" --mail-user="$MAIL_USER" $WF_DEMULTMT $SAMPLE_ID)
+	echo "> Submitted batch job $JOBID"
+	echo "  Output in $SLURM_FILE"
+	
+# 	WF_ID='modmito'
+# 	SLURM_FILE="$OUT_PATH/$SLURM_PRE.$WF_ID.$SLURM_EXT"
+# 	JOBID=(sbatch --dependency=afterok:${JOBID} --chdir="$RUN_DIR_PATH" --job-name="${WF_ID:0:1}${SAMPLE_ID: -5}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_CUSTOM" --mail-user="$MAIL_USER" $WF_MODMITO $SAMPLE_ID)
+# 	echo "> Submitted batch job $JOBID"
+# 	echo "  Output in $SLURM_FILE"
 
-SLURM_FILE="$RUN_DIR_PATH/$OUT_DIR/$SLURM_PRE.modmito.$SLURM_EXT"
-sbatch --dependency=afterok:${JOBID} --chdir=$RUN_DIR_PATH --job-name="m${SAMPLE_ID: -5}" --output="$SLURM_FILE" $WF_MODMITO $SAMPLE_ID
-echo "> Output in $SLURM_FILE"
+done
