abf4c730b901698143118319051dc4f918133e8b
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2025-05-12 18:12:43 +0200

    Upgrade with symlink to dorado

--- wf-bchg.sh
+++ wf-bchg.sh
@@ -12,7 +12,7 @@
 # wf-bchg.sh /Path/to/run/dir/
 #
 #
-VERSION='25.05.12.1'
+VERSION='25.05.12.2'
 
 AUTHOR='Marc FERRE <marc.ferre@univ-angers.fr>'
 
@@ -34,7 +34,7 @@ MODEL='sup'
 KIT='SQK-NBD114-24'
 
 # Binary and Conda env
-DORADO_BIN='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/bioapp/dorado-0.9.1-linux-x64/bin/dorado'
+DORADO_BIN='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/bioapp/dorado'
 
 check_dir () { 
 	if [ -d "$1" ]
