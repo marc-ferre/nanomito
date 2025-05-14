348cca4657999a6df2a067520316496d7b1e3eee
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2025-05-14 09:05:48 +0200

    Various improvements after shellcheck + Update DORADO_BIN

--- wf-modmito.sh
+++ wf-modmito.sh
@@ -12,7 +12,7 @@
 # wf-modmito.sh /Path/to/run/dir/
 #
 #
-VERSION='25.03.25.7'
+VERSION='25.05.14.1'
 
 AUTHOR='Marc FERRE <marc.ferre@univ-angers.fr>'
 
@@ -24,7 +24,7 @@ RUN_ID=${RUN_ID:-/} # Correct for the case where PWD=/
 if [ $# -eq 0 ]
 	then
 		echo "[ERROR] No arguments supplied"
-		exit 9999 # die with error code 9999
+		exit 128 # die with error code
 fi
 SAMPLE_ID=$1
 
@@ -35,14 +35,14 @@ SELECT='both'
 MODEL_COMPLEX='sup,5mC_5hmC,6mA'
 
 # Directories
-RUN_DIR=`pwd`
+RUN_DIR=$(pwd)
 PROCESS_DIR="$RUN_DIR/processing"
 OUT_DIR="$PROCESS_DIR/$SAMPLE_ID"
 SELECT_DIR="$OUT_DIR/select-$SELECT"
 # MODBASE_DIR="$OUT_DIR/modbase"
 
 # Binary and Conda env
-DORADO_BIN='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/bioapp/dorado-0.9.1-linux-x64/bin/dorado'
+DORADO_BIN='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/bioapp/dorado-0.9.6-linux-x64/bin/dorado'
 MODMITO_ENV='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/bioapp/env_modmito'
 
 # Reference
@@ -56,8 +56,7 @@ DEMULT_POD5_FILE="$SELECT_DIR/$SAMPLE_ID.demultmt.pod5"
 BAM_FILE="$OUT_DIR/$BAM_PREFIX.bam"
 SORTED_BAM_FILE="$OUT_DIR/$BAM_PREFIX.sorted.bam"
 BEDMETHYL_FILE="$OUT_DIR/$BAM_PREFIX.combine.bed"
-PILEUP_LOG_FILE="$OUT_DIR/$BAM_PREFIX.pileup.log"
-SAMPLESHEET_FILE=`readlink -f "$(find $RUN_DIR -type f -name 'sample_sheet_*.csv')"`
+SAMPLESHEET_FILE=$(readlink -f "$(find "$RUN_DIR" -type f -name 'sample_sheet_*.csv')")
 WORKFLOW_SUMMARY_FILE="$PROCESS_DIR/workflows_summary.$RUN_ID.tsv"
 
 check_dir () { 
@@ -66,7 +65,7 @@ check_dir () {
    		echo "[OK] Directory $1 exists"
    	else
 		echo "[ERROR] Directory $1 doesn't exist"
-		exit 9999 # die with error code 9999
+		exit 128 # die with error code 
 	fi
 }
 check_file () { 
@@ -75,11 +74,11 @@ check_file () {
    		echo "[OK] File $1 exists and is not empty"
    	else
 		echo "[ERROR] File $1 is empty or doesn't exist"
-		exit 9999 # die with error code 9999
+		exit 128 # die with error code
 	fi
 }
 
-START=`date +%s`
+START=$(date +%s)
 
 echo "Workflow: wf-modmito v.$VERSION by $AUTHOR"
 echo "Run: $RUN_ID"
@@ -92,13 +91,13 @@ echo "Pod5 file: $DEMULT_POD5_FILE"
 echo "Sample sheet: $SAMPLESHEET_FILE"
 echo "Model Complex: $MODEL_COMPLEX"
 echo "Reference file: $SELECTED_REF"
-echo "Date: `date`"
+echo "Date: $(date)"
 
 echo
 echo '******************************'
 echo '* Modified bases basecalling *'
 echo '******************************'
-check_file $DEMULT_POD5_FILE
+check_file "$DEMULT_POD5_FILE"
 
 # To work around the issue https://github.com/nanoporetech/dorado/issues/432
 export LC_ALL=en_US.UTF-8
@@ -106,34 +105,36 @@ export LC_ALL=en_US.UTF-8
 echo "Dorado version:"
 $DORADO_BIN --version
 
-$DORADO_BIN duplex $MODEL_COMPLEX $DEMULT_POD5_FILE \
+$DORADO_BIN duplex $MODEL_COMPLEX "$DEMULT_POD5_FILE" \
 	--verbose \
 	--reference $SELECTED_REF \
-	> $BAM_FILE
-check_file $BAM_FILE
+	> "$BAM_FILE"
+check_file "$BAM_FILE"
 
 echo
 echo '****************************'
 echo '* Sorted BAM and bedMethyl *'
 echo '****************************'
 
+# Source Conda, to use it on a Genouest cluster compute node
 . /local/env/envconda.sh
+
 conda activate $MODMITO_ENV
 
-echo "`samtools --version`"
+samtools --version
 
-samtools sort $BAM_FILE -o $SORTED_BAM_FILE
-check_file $SORTED_BAM_FILE
-samtools index $SORTED_BAM_FILE
+samtools sort "$BAM_FILE" -o "$SORTED_BAM_FILE"
+check_file "$SORTED_BAM_FILE"
+samtools index "$SORTED_BAM_FILE"
 check_file "${SORTED_BAM_FILE}.bai"
 
 echo "Remove unsorted BAM file:"
-rm -i $BAM_FILE && [[ ! -e $BAM_FILE ]] && echo "[OK] BAM file removed: $BAM_FILE"
+rm -i "$BAM_FILE" && [[ ! -e $BAM_FILE ]] && echo "[OK] BAM file removed: $BAM_FILE"
 
-echo "Modkit version: `modkit --version`"
+echo "Modkit version: $(modkit --version)"
 
-modkit pileup $SORTED_BAM_FILE $BEDMETHYL_FILE #--log-filepath $PILEUP_LOG_FILE
-check_file $BEDMETHYL_FILE
+modkit pileup "$SORTED_BAM_FILE" "$BEDMETHYL_FILE"
+check_file "$BEDMETHYL_FILE"
 
 conda deactivate
 
@@ -142,8 +143,8 @@ echo '***********'
 echo '* Ending  *'
 echo '***********'
 
-END=`date +%s`
-RUNTIME=$(echo "$END - $START")
+END=$(date +%s)
+RUNTIME=$((END - START))
 HOURS=$((RUNTIME / 3600))
 MINUTES=$(( (RUNTIME % 3600) / 60 ))
 SECONDS=$(( (RUNTIME % 3600) % 60 ))
@@ -151,8 +152,8 @@ echo ">>> Runtime: $HOURS:$MINUTES:$SECONDS (hh:mm:ss)"
 
 # Write workflow summary file
 if ! [ -e "$WORKFLOW_SUMMARY_FILE" ] ; then
-	echo "Run id	Sample id	Workflow	Runtime (hh:mm:ss)" > $WORKFLOW_SUMMARY_FILE
+	echo "Run id	Sample id	Workflow	Runtime (hh:mm:ss)" > "$WORKFLOW_SUMMARY_FILE"
 	echo "[OK] File $WORKFLOW_SUMMARY_FILE created (with header)"
 fi
-echo "$RUN_ID	$SAMPLE_ID	modmito 	$HOURS:$MINUTES:$SECONDS" >> $WORKFLOW_SUMMARY_FILE
+echo "$RUN_ID	$SAMPLE_ID	modmito 	$HOURS:$MINUTES:$SECONDS" >> "$WORKFLOW_SUMMARY_FILE"
 echo "[OK] Line added to $WORKFLOW_SUMMARY_FILE"
