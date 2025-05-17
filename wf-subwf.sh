076f63937749d9d7af2d79a80d4489acccbea12f
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2025-05-17 14:17:36 +0200

    Improvements after shellcheck

--- wf-subwf.sh
+++ wf-subwf.sh
@@ -5,22 +5,22 @@
 #SBATCH --mail-user=marc.ferre@univ-angers.fr
 #
 #
-# wf-subwf.sh /Path/to/run/dir/
+# wf-subwf.sh
 #
 #
 set -e
 
-VERSION='25.03.27.1'
+VERSION='25.05.17.1'
 
 AUTHOR='Marc FERRE <marc.ferre@univ-angers.fr>'
 
 # Directories
-RUN_DIR=`pwd`
+RUN_DIR=$(pwd)
 FASTQ_DIR="$RUN_DIR/fastq_pass"
 PROCESS_DIR="$RUN_DIR/processing"
 
 # Prefixes
-RUN_ID=`basename $RUN_DIR`
+RUN_ID=$(basename "$RUN_DIR")
 
 # Files
 WORKFLOW_SUMMARY_FILE="$PROCESS_DIR/workflows_summary.$RUN_ID.tsv"
@@ -31,12 +31,12 @@ WF_MODMITO='/home/genouest/cnrs_umr6015_inserm_umr1083/mferre/workflows/wf-modmi
 
 # Mail options
 MAIL_USER='marc.ferre@univ-angers.fr'
-MAIL_TYPE_ALL='ALL'
 MAIL_TYPE_END='END,FAIL,INVALID_DEPEND,REQUEUE,STAGE_OUT,TIME_LIMIT_90'
 MAIL_TYPE_ISSUE='FAIL,INVALID_DEPEND,REQUEUE,STAGE_OUT,TIME_LIMIT_90'
-MAIL_TYPE_NONE='NONE'
+# MAIL_TYPE_NONE='NONE'
+# MAIL_TYPE_ALL='ALL'
 
-START=`date +%s`
+START=$(date +%s)
 
 echo "=== Submit workflows to Slurm ==="
 echo "Workflow: wf-subwf v.$VERSION by $AUTHOR"
@@ -45,14 +45,16 @@ echo "Job: $SLURM_JOB_ID"
 echo "Run dir   : $RUN_DIR"
 echo "FastQ dir : $FASTQ_DIR"
 echo "Output dir: $PROCESS_DIR"
-echo "Date: `date`"
+echo "Date: $(date)"
 
 SAMPLES_COUNT=0
 JOBS_COUNT=0
 JOBID_LIST=''
-cd $FASTQ_DIR
-for DIR in $(ls -1 -d */); do
-	SAMPLE_ID=`basename $DIR`
+cd "$FASTQ_DIR"
+for DIR in $(ls -1 -d ./*/)
+do
+	[[ -d "$DIR" ]] || break
+	SAMPLE_ID=$(basename "$DIR")
 	echo "--- Sample: $SAMPLE_ID"
 	
 	SAMPLES_COUNT=$((SAMPLES_COUNT+1))
@@ -63,7 +65,7 @@ for DIR in $(ls -1 -d */); do
 	
 	WF_ID='demultmt'
 	SLURM_FILE="$OUT_DIR/$SLURM_PRE.$WF_ID.$SLURM_EXT"
-	JOBID=$(sbatch --parsable --chdir="$RUN_DIR" --job-name="${WF_ID:0:1}${SAMPLE_ID: -7}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_ISSUE" --mail-user="$MAIL_USER" $WF_DEMULTMT $SAMPLE_ID)
+	JOBID=$(sbatch --parsable --chdir="$RUN_DIR" --job-name="${WF_ID:0:1}${SAMPLE_ID: -7}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_ISSUE" --mail-user="$MAIL_USER" $WF_DEMULTMT "$SAMPLE_ID")
 	echo "> Submitted batch job $JOBID"
 	echo "  Output in $SLURM_FILE"
 	JOBS_COUNT=$((JOBS_COUNT+1))
@@ -71,7 +73,7 @@ for DIR in $(ls -1 -d */); do
 	
 	WF_ID='modmito'
 	SLURM_FILE="$OUT_DIR/$SLURM_PRE.$WF_ID.$SLURM_EXT"
- 	JOBID=$(sbatch  --dependency=afterok:${JOBID} --parsable --chdir="$RUN_DIR" --job-name="${WF_ID:0:1}${SAMPLE_ID: -7}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_END" --mail-user="$MAIL_USER" $WF_MODMITO $SAMPLE_ID)
+ 	JOBID=$(sbatch  --dependency=afterok:"${JOBID}" --parsable --chdir="$RUN_DIR" --job-name="${WF_ID:0:1}${SAMPLE_ID: -7}" --output="$SLURM_FILE" --mail-type="$MAIL_TYPE_END" --mail-user="$MAIL_USER" $WF_MODMITO "$SAMPLE_ID")
 	echo "> Submitted batch job $JOBID"
 	echo "  Output in $SLURM_FILE"
 	JOBS_COUNT=$((JOBS_COUNT+1))
@@ -88,8 +90,8 @@ echo "|"
 echo "|"
 echo "|"
 
-END=`date +%s`
-RUNTIME=$(echo "$END - $START")
+END=$(date +%s)
+RUNTIME=$((END - START))
 HOURS=$((RUNTIME / 3600))
 MINUTES=$(( (RUNTIME % 3600) / 60 ))
 SECONDS=$(( (RUNTIME % 3600) % 60 ))
@@ -99,8 +101,8 @@ echo "|"
 
 # Write workflow summary file
 if ! [ -e "$WORKFLOW_SUMMARY_FILE" ] ; then
-	echo "Run id	Sample id	Workflow	Runtime (hh:mm:ss)" > $WORKFLOW_SUMMARY_FILE
+	echo "Run id	Sample id	Workflow	Runtime (hh:mm:ss)" > "$WORKFLOW_SUMMARY_FILE"
 	echo "[OK] File $WORKFLOW_SUMMARY_FILE created (with header)"
 fi
-echo "$RUN_ID		subwf	$HOURS:$MINUTES:$SECONDS" >> $WORKFLOW_SUMMARY_FILE
+echo "$RUN_ID		subwf	$HOURS:$MINUTES:$SECONDS" >> "$WORKFLOW_SUMMARY_FILE"
 echo "[OK] Line added to $WORKFLOW_SUMMARY_FILE"
