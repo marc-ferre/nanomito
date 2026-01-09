5917105b7e424ebd413a7b8d73b0d9b3541a62b4
Marc Ferré <70011606+marc-ferre@users.noreply.github.com>
2026-01-09 11:30:30 +0100

    Fix: Robust TSV parsing by forcing FS=tab (ASCII 9) in awk BEGIN\n\n- Avoids shell quoting issues with \t\n- Prevents splitting headers/data on spaces in haplogroup table

--- wf-finalize.sh
+++ wf-finalize.sh
@@ -406,6 +406,8 @@ EOVCF
     function toup(x,  i,c,r){ r=""; for(i=1;i<=length(x);i++){c=substr(x,i,1); r=r ((c>="a" && c<="z")? sprintf("%c", ord(c)-32): c)}; return r }
     function ord(c){ return index("\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !\"#$%&\047()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~", c)-1 }
     BEGIN {
+      # Force tab as field separator robustly (ASCII 9), regardless of shell quoting
+      FS = sprintf("%c", 9)
       filter_idx=0; alt_idx=0; end_idx=0; svlen_idx=0;
       
       # Load tooltips from file if provided
